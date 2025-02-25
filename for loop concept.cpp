8e69da061c56f6d576da38bba572a0d3833b065b
Ramesh Shahi <rameshshahi714@gmail.com>
2025-02-25 15:45:51 +0545

    Update for loop concept.cpp

--- for loop concept.cpp
+++ for loop concept.cpp
@@ -12,16 +12,16 @@ using namespace std;
 ********
      */
 
-// int main(){
-//     for(int i=1;i<=8;i++){
-//         for(int j=1;j<i;j++){
-//             printf("*");
-//         }
-//         printf("\n");
-//     }
+ int main(){
+     for(int i=1;i<=8;i++){
+         for(int j=1;j<i;j++){
+             printf("*");
+         }
+         printf("\n");
+     }
 
-//     return 0;
-// }
+     return 0;
+ }
 
 
 
@@ -36,15 +36,15 @@ using namespace std;
 *
 */
 
-// int main(){
-//     for(int i=1;i<=8;i++){
-//         for(int j=8;i<=j;j--){
-//             printf("f");
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
+ int main(){
+     for(int i=1;i<=8;i++){
+         for(int j=8;i<=j;j--){
+             printf("f");
+         }
+         printf("\n");
+     }
+     return 0;
+ }
 
 
 
@@ -63,20 +63,20 @@ using namespace std;
 
 */
 
-// int main(){
-//     for(int i=1;i<=11;i++){
-//         for(int j=i;j<11;j++){
-//             printf(" ");
-//         }
+ int main(){
+     for(int i=1;i<=11;i++){
+         for(int j=i;j<11;j++){
+             printf(" ");
+         }
         
-//         for(int k=1;k<=i;k++){
-//             printf("*");
-//         }
-//         printf("\n");
+         for(int k=1;k<=i;k++){
+             printf("*");
+         }
+         printf("\n");
 
-//     }
-//     return 0;
-// }
+     }
+     return 0;
+ }
     
 
 
@@ -92,16 +92,16 @@ using namespace std;
 *******
 */
 
-// int main(){
-//     for(int i=1;i<=9;i++){
-//         for(int j=1;j<10;j++){
-//             printf("*");
-//         }
-//         printf("\n");
+ int main(){
+     for(int i=1;i<=9;i++){
+         for(int j=1;j<10;j++){
+             printf("*");
+         }
+         printf("\n");
 
-//     }
-//     return 0;
-// }
+     }
+     return 0;
+ }
       
 
 /*5.
@@ -116,20 +116,20 @@ using namespace std;
 *********
 */
     
-// int main(){
-//     for(int i=1;i<=9;i++){
-//         for(int j=1;j<=9;j++){
-//             if(i==1||i==9||j==1||j==9){
-//                 printf("*");
-//             }
-//             else{
-//                 printf(" ");
-//             }
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
+ int main(){
+     for(int i=1;i<=9;i++){
+         for(int j=1;j<=9;j++){
+             if(i==1||i==9||j==1||j==9){
+                 printf("*");
+             }
+             else{
+                 printf(" ");
+             }
+         }
+         printf("\n");
+     }
+     return 0;
+ }
 
 
 
@@ -143,19 +143,19 @@ using namespace std;
  * * * * * * * * *
 */
 
-// int main(){
-//     for(int i=1;i<=5;i++){
-//         for(int j=i;j<5;j++){
-//             cout<<" ";
-//         }
-//         for(int k=1;k<2*i;k++){
-//             cout<<"*";
-//         }
-//         cout<<"\n";
+ int main(){
+     for(int i=1;i<=5;i++){
+         for(int j=i;j<5;j++){
+             cout<<" ";
+         }
+         for(int k=1;k<2*i;k++){
+             cout<<"*";
+         }
+         cout<<"\n";
 
-//     }
-//     return 0;
-// }
+     }
+     return 0;
+ }
 
 
 
@@ -177,29 +177,29 @@ using namespace std;
 
 
 
-// int main(){
-//     for(int i=1;i<=5;i++){
-//         for(int j=i;j<5;j++){
-//             cout<<" ";
-//         }
-//         for(int k=1;k<2*i;k++){
-//             cout<<"*";
-//         }
-//         cout<<"\n";
+ int main(){
+     for(int i=1;i<=5;i++){
+         for(int j=i;j<5;j++){
+             cout<<" ";
+         }
+         for(int k=1;k<2*i;k++){
+             cout<<"*";
+         }
+         cout<<"\n";
         
-//     }
-//     // cout<<"\n";
-//     for(int l=1;l<=4;l++){
-//         for(int m=1;m<=l;m++){
-//             cout<<" ";
-//         }
-//         for(int o=8;o>=2*l;o--) {
-//             cout<<"*";
-//         }
-//         cout<<"\n";
-//     }
-//     return 0;
-// }
+     }
+     // cout<<"\n";
+     for(int l=1;l<=4;l++){
+         for(int m=1;m<=l;m++){
+             cout<<" ";
+         }
+         for(int o=8;o>=2*l;o--) {
+             cout<<"*";
+         }
+         cout<<"\n";
+     }
+     return 0;
+ }
 
 
 
@@ -215,25 +215,25 @@ using namespace std;
 
 
 
-// int main(){
-//   for(int i=1;i<=4;i++){
-//     for(int j=i;j<=4;j++){
-//       cout<<" ";
-//     }
-//     for(int k=1;k<=i;k++){
-//       cout<<"*";
-//     }
-//     cout<<"\n";
+ int main(){
+   for(int i=1;i<=4;i++){
+     for(int j=i;j<=4;j++){
+       cout<<" ";
+     }
+     for(int k=1;k<=i;k++){
+       cout<<"*";
+     }
+     cout<<"\n";
     
-//   }
-//   for(int l=1;l<=3;l++){
-//     for(int m=0;m<=l;m++){
-//       cout<<" ";
-//     }
-//     for(int n=3;n>=l;n--){
-//       cout<<"*";
-//     }
-//     cout<<"\n";
-//   }
-//   return 0;
-// }
+   }
+   for(int l=1;l<=3;l++){
+     for(int m=0;m<=l;m++){
+       cout<<" ";
+     }
+     for(int n=3;n>=l;n--){
+       cout<<"*";
+     }
+     cout<<"\n";
+   }
+   return 0;
+ }
